a68338fa2998dc4c57334afdb584f2710d4dfbea
Matt Gilchrist <mag@Matthews-MacBook-Air.local>
2019-08-28 17:22:13 -0300

    Table data gets sorted and placed in correct columns

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
-// document.getElementById('updateTable').addEventListener('click', (e) => {
-//   const xhr = new XMLHttpRequest();
-//
-//   xhr.open('GET', 'players.json', true);
-//   xhr.onload = function () {
-//     if (this.status === 200) {
-//
-//       const players = JSON.parse(this.responseText);
-//       let output;
-//       players.forEach((player) =>{
-//         output = `<td>${player.name}</td>
-//                    <td>${player.wins}</td>
-//                    <td>${player.totalPoints}</td>`
-//         document.getElementById(`${player.id}`).innerHTML = output;
-//       })
-//     }
-//   }
-//   xhr.send();
-// });
+document.getElementById('updateTable').addEventListener('click', (e) => {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('GET', 'players.json', true);
+  xhr.onload = function () {
+    if (this.status === 200) {
+
+      const players = JSON.parse(this.responseText);
+      console.log(players);
+      players.sort((a, b) => b.totalPoints - a.totalPoints);
+
+      console.log(players);
+      let output;
+      let count = 1;
+      players.forEach((player) =>{
+        output = ` <td>${count}</td>
+                   <td>${player.name}</td>
+                   <td>${player.wins}</td>
+                   <td>${player.totalPoints}</td>`
+        document.getElementById(count).innerHTML = output;
+        count++;
+      })
+    }
+  }
+  xhr.send();
+});
 
 
 class Player {
@@ -43,8 +50,18 @@ class Player {
   setTotalPoints(totalPoints) {
     this.totalPoints = totalPoints;
   }
+
+  sortbyPoints(players) {
+
+  }
 }
 
 class UI {
+
+}
+
+
+// localStorage class
+class Store {
 
 }
