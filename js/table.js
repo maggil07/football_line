125a74e00b603155a47c7d5752ae5d26f3ca5ce0
maggil07 <mag738@mun.ca>
2019-10-08 21:59:20 -0230

    week5

--- js/table.js
+++ js/table.js
@@ -47,6 +47,7 @@ document.getElementById('updateTable').addEventListener('click', (e) => {
                    <td>${player.week2}</td>
                    <td>${player.week3}</td>
                    <td>${player.week4}</td>
+                   <td>${player.week5}</td>
                    <td>${player.totalPoints}</td>`
         document.getElementById(count).innerHTML = output;
         count++;
